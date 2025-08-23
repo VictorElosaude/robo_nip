c8fe3444e23f3f453a593875742a699fd5fab141
VictorElosaude <victor@elosaude.com.br>
2025-08-23 12:35:42 -0300

    Update novo.py
    impedir que a tela seja do navegador seja exibida

--- novo.py
+++ novo.py
@@ -26,6 +26,7 @@ def highlight(element, driver):
 def setup_browser():
     """Configura o navegador Chrome com opções anti-bot."""
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
